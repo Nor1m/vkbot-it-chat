bfd41eabf6aa3bf7745d4317edfa0b8d9fa7a6d8
Roman Mitasov <metas_roman@mail.ru>
2018-11-08 02:37:28 +0300

    Настроил автозагрузку в композере 2
    Исправил путь в параметрах. Убрал мусор из bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-require_once "bot/Bot.php";
 use VkBot\Bot;
 
 if (!isset($_REQUEST)) {
