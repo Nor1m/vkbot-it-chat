9ec47d91ba213fa93b6371c2209999d45eba1977
Nor1m <nor1moft@mail.ru>
2018-11-08 06:06:35 +0600

    UPD

--- bot.php
+++ bot.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once "bot/Bot.php";
 use VkBot\Bot;
 
 if (!isset($_REQUEST)) {
@@ -14,7 +19,5 @@ $bot->secretKey       = VK_SECRET_KEY;
 $bot->confirmationKey = VK_CONFIRMATION_TOKEN;
 $bot->run(json_decode(
     file_get_contents('php://input'),
-    false,
-    512,
-    JSON_THROW_ON_ERROR
+    false
 ));
