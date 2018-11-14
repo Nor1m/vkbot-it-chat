93c920a9d0a4e4a49fd4f5cc944e4270a526738d
Nor1m <nor1moft@mail.ru>
2018-11-15 04:30:10 +0600

    Обновлена конфигурация

--- app/config/env.sample.php
+++ app/config/env.sample.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 // Конфигурация бота
 
 /**
- * @var string Дебаг. Если всключён, будут работать логи и прочие плюхи
+ * @var string Дебаг. Если включён, будут работать логи и прочие плюхи
  */
 const APP_DEBUG = false;
 
@@ -18,9 +18,9 @@ const BOT_STATUS = true;
 // Конфигурация VK
 
 /**
- * @var string ID группы ВК
+ * @var int ID группы ВК
  */
-const VK_GROUP_ID = 0;
+const VK_GROUP_ID = ;
 
 /**
  * @var string Токен доступа сообщества
@@ -42,12 +42,7 @@ const VK_CONFIRMATION_TOKEN = "";
  * @var string Путь к корню проекта
  */
 defined("ROOT_PATH")
-or define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . "/vkapi/" );
-
-/**
- * @var string URL API Wikipedia
- */
-const WIKI_API_URL = "https://ru.wikipedia.org/w/api.php?";
+or define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . "/vkapi2/" );
 
 // Конфигурация бд
 
@@ -65,3 +60,22 @@ const DB_USERNAME = "";
  * @var string Пароль базы данных
  */
 const DB_PASSWORD = "";
+
+// Конфигурация API Wikipedia
+
+/**
+ * @var string URL API Wikipedia
+ */
+const WIKI_API_URL = "https://ru.wikipedia.org/w/api.php?";
+
+// Конфигурация API YANDEX TRANSLATE
+
+/**
+ * @var string URL API YANDEX TRANSLATE
+ */
+const YANDEXTRANSLATE_API_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
+
+/**
+ * @var string API KEY YANDEX TRANSLATE
+ */
+const YANDEXTRANSLATE_API_KEY = "";
